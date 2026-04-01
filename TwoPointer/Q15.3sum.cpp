2077a098e6351db05ed15510e3d92b5933e6b7df
coderRohit620 <rohitravi620@gmail.com>
2026-04-01 09:06:49 +0530

    update 3sum or remove redundant break condition and improve clarity

--- TwoPointer/Q15.3sum.cpp
+++ TwoPointer/Q15.3sum.cpp
@@ -39,8 +39,6 @@ class Solution1 {
         for(int i = 0 ; i < n-2 ; i++){
             if( i > 0 && nums[i] == nums[i-1]) continue;
 
-            if(nums[i] > 0) break;
-
             int left = i+1, right = n-1;
             int target  = -nums[i];
 
@@ -59,7 +57,38 @@ class Solution1 {
             }
         }
         return res;
-        
+    }
+};
+
+
+// Optimal Approch - sorting + two pointer
+// NOTE: we can break the loop if nums[i] > 0 because in that case sum will always be greater than 0
+class Solution1 {
+    public:
+    vector<vector<int>> threeSum(vector<int>& nums){
+        int n = nums.size();
+        vector<vector<int>> res;
+        for(int i = 0 ; i < n-2 ; i++){
+            if( i > 0 && nums[i] == nums[i-1]) continue;
+
+            if(nums[i] > 0) break;
+
+            int left = i+1, right = n-1;
+
+            while(left < right){
+                int sum = nums[i]+nums[left]+nums[right];
+
+                if(sum == 0){
+                    res.push_back({nums[i],nums[left],nums[right]});
+
+                    while(left < right && nums[left]==nums[left-1])   left++;
+                    while(left < right && nums[right]==nums[right+1])   right--;
+                }
+                else if(sum > 0) left++;
+                else right--;
+            }
+        }
+        return res;
     }
 };
 
